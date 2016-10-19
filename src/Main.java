b7ee92498aabe4889d552172a6c7f70dbd616082
gracenotegracenote <L.Kachurina@campus.lmu.de>
2016-10-19 23:04:43 +0200

    printHistogram method created

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Author: Liudmila Kachurina (https://github.com/gracenotegracenote)
+ * Date: 19-Oct-16
+ */
+public class Main {
+	public static void main(String[] args) {
+		double[] data = {1,2,3};
+
+		printHistogram(data);
+	}
+
+	private static void printHistogram(double[] data) {
+		System.out.println("Hey");
+	}
+}
